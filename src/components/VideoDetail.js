b6e2bc3dac5e9a58d1dccaa8a2e8e5c6420bcc9d
k1yeon <gio13remanente@gmail.com>
2021-05-28 06:06:37 +0800

    displaying apostrophes

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -8,10 +8,16 @@ const VideoDetail = ({ video }) => {
 	return (
 		<div>
 			<div className="ui embed">
-				<iframe src={videoSrc} allowFullScreen title="video player" />
+				<iframe
+					src={`${videoSrc}?autoplay=1`}
+					allowFullScreen
+					title="video player"
+				/>
 			</div>
 			<div className="ui segment">
-				<h4 className="ui header">{video.snippet.title}</h4>
+				<h4 className="ui header">
+					{video.snippet.title.replaceAll('&#39;', "'")}
+				</h4>
 				<p>{video.snippet.description}</p>
 			</div>
 		</div>
--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -6,11 +6,13 @@ const VideoItem = ({ onVideoSelect, video }) => {
 		<div onClick={() => onVideoSelect(video)} className="video-item item">
 			<img
 				className="ui image"
-				alt={video.snippet.title}
+				alt={video.snippet.title.replaceAll('&#39;', "'")}
 				src={video.snippet.thumbnails.medium.url}
 			/>
 			<div className="content">
-				<div className="header">{video.snippet.title}</div>
+				<div className="header">
+					{video.snippet.title.replaceAll('&#39;', "'")}
+				</div>
 			</div>
 		</div>
 	);
